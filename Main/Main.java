915f0a4162002895fef2cc960bd670d6f89f3fb0
vivekdunuka <dunukavivek@gmial.com>
2026-01-29 15:05:35 +0530

    first commit

---
+++ Main/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s="0123456789";
+        System.out.println(s.length());
+        // String sa=sc.nextLine();
+        // String[] sb=sa.split(" ");
+        // for(int str=0;str<sb.length;str++){
+        //     if(s.indexOf(sb[str].charAt(0))!=-1){
+        //         StringBuilder a= new StringBuilder(sb[str].substring(1,sb[str].length()));
+        //         sb[str]= a.reverse().toString();
+        //     }
+        // }
+        // sa=String.join(" ",sb);
+        // System.out.println(sa);
+    }
+}
